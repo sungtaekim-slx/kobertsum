429c4458b214380eb7681f5d551d26febc63bac4
aiKSTlab <28707018+aiKSTlab@users.noreply.github.com>
2022-01-12 09:18:08 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get update
-apt-get upgrade
-apt install wget
-apt install curl
-apt install git
+apt-get -y update
+apt-get -y upgrade
+apt install -y wget
+apt install -y curl
+apt install -y git
 
 python main.py -task install
